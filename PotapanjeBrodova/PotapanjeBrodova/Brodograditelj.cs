ea8dfaca5faa571e2f341d1fedbbbb2c0d4ff76b
Marijo Horvat <Marijo.Horvat@vsite.hr>
2017-04-12 19:53:17 +0200

    Brodograditelj dodan

---
+++ PotapanjeBrodova/PotapanjeBrodova/Brodograditelj.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Brodograditelj
+    {
+        public Flota SloziFlotu(Mreža mreža, IEnumerable<int> duljinaBrodova)
+        {
+            Flota flota = new Flota();
+            TerminatorPolja termi = new TerminatorPolja(mreža);
+
+            foreach (int i in duljinaBrodova)
+            {
+                var nizovi = mreža.DajNizoveSlobodnihPolja(i);
+                int index = slucajniBrojevi.Next(nizovi.Count());
+                var niz = nizovi.ElementAt(index);
+                flota.DodajBrod(niz);
+                termi.UkloniPolja(niz);
+            }
+            return flota;
+        }
+
+        // TODO obratiti paznju za slucaj da se ne mogu svi brodovi sloziti.
+
+        private Random slucajniBrojevi = new Random();
+    }
+}
---
+++ PotapanjeBrodova/Test/TestBrodograditelja.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class TestBrodograditelja
+    {
+        [TestMethod]
+        public void Brodograditelj_SloziFlotuVeacaFlotuSBrodovimaZadaneDuljine()
+        {
+            Brodograditelj b = new Brodograditelj();
+            Mreža mreža = new Mreža(10, 10);
+            IEnumerable<int> duljinaBrodova = new int[] { 5, 4, 4, 3, 3, 3, 2, 2,2,2 };
+            Flota f = b.SloziFlotu(mreža, duljinaBrodova);
+            Assert.AreEqual(duljinaBrodova.Count(), f.BrojBrodova);
+        }
+    }
+}
