5bf518679cd3e1561e574673d23ad0817c718498
Marijo Horvat <Marijo.Horvat@vsite.hr>
2017-05-10 16:41:23 +0200

    Dodane klase za 3 nacina pucanja

---
+++ PotapanjeBrodova/PotapanjeBrodova/LinijskiPucač.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class LinijskiPucač : IPucač
+    {
+        public Polje Gađaj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObradiGađanja(RezultatGađanja rezultat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotapanjeBrodova/PotapanjeBrodova/SlučajniPucač.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class SlučajniPucač : IPucač
+    {
+        public Polje Gađaj()
+        {
+            // random gadjanje, mora dobiti mrezu od topnistva, od mreze trazi najdulji brod (konstruktor)
+            // i trazi polja gdje moze staviti taj najdulji brod. Tezina polja +
+            throw new NotImplementedException();
+        }
+
+        public void ObradiGađanja(RezultatGađanja rezultat)
+        {
+            // ako je brod potopljen, eliminira polja oko broda
+            throw new NotImplementedException();
+        }
+    }
+}
