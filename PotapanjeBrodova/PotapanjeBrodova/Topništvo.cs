2fcd7fff31f9fd5ba400732526be00378f30cdc5
Marijo Horvat <Marijo.Horvat@vsite.hr>
2017-05-10 20:09:39 +0200

    Topnistvo.cs  - Nadopunjeno pozivanje taktika

--- PotapanjeBrodova/PotapanjeBrodova/Topništvo.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Topništvo.cs
@@ -52,14 +52,19 @@ namespace PotapanjeBrodova
        private  void PromijeniTaktikuUKružno()
         {
             TaktikaGađanja = TaktikaGađanja.Kružno;
+            Polje pogođenoPolje = pucač.PogođenaPolja.First();
+            pucač = new KružniPucač(mreza, pogođenoPolje, duljineBrodova.First());
         }
         private void PromijeniTaktikuULinijsko()
         {
             TaktikaGađanja = TaktikaGađanja.Linijsko;
+            var pogođenoPolje = pucač.PogođenaPolja;
+            pucač = new LinijskiPucač(mreza, pogođenoPolje, duljineBrodova.First());
         }
         private void PromijeniTaktikuUNasumično()
         {
             TaktikaGađanja = TaktikaGađanja.Nasumično;
+            pucač = new SlučajniPucač(mreza, duljineBrodova.First());
         }
 
         public TaktikaGađanja TaktikaGađanja { get; private set; }
